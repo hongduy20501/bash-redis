a2d14a33b1bcfd99e9ce2ecf181979edd3065fa3
hongduy20501 <72614920+hongduy20501@users.noreply.github.com>
2023-05-19 10:15:00 +0700

    Update setupredis-slave.sh

--- setupredis-slave.sh
+++ setupredis-slave.sh
@@ -14,7 +14,7 @@ fi
 
 sudo sed -i -E 's/(bind) ([0-9.]+) (::1)/\1 0.0.0.0 ''/' /etc/redis/redis.conf
 sudo sed -i -E 's/#? (requirepass) (foobared)/\1 hongduy/' /etc/redis/redis.conf
-sudo sed -i -E 's/#? (masterauth) (master-password>)/\1 hongduy/' /etc/redis/redis.conf
+sudo sed -i -E 's/#? (masterauth) (<master-password>)/\1 hongduy/' /etc/redis/redis.conf
 #A=$(ip route list | grep 'default' | grep -oE 'dev [a-z0-9]+' | awk '{print $2}')
 #B=$(ip address show $A | grep inet | grep -Eo '[0-9.]+' | head -1)
 sudo sed -i -E 's/#? (replicaof) .*/\1 10.207.127.170 6379/' /etc/redis/redis.conf

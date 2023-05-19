9cf3b980b7ded7f3b536d6fa15643324ecd91c31
hongduy20501 <72614920+hongduy20501@users.noreply.github.com>
2023-05-19 10:13:56 +0700

    Update setupredis-master.sh

--- setupredis-master.sh
+++ setupredis-master.sh
@@ -7,13 +7,13 @@ if [ -z "$(which redis)" ]; then
   
 fi
 
-if [ $(service redis-server status | grep -c running ) != 1]; then
+if [ $(service redis-server status | grep -c running) != 1]; then
   echo 'error'
   exit 1
 fi
 
 sudo sed -i -E 's/(bind) ([0-9.]+) (::1)/\1 0.0.0.0 ''/' /etc/redis/redis.conf
 sudo sed -i -E 's/#? (requirepass) (foobared)/\1 hongduy/' /etc/redis/redis.conf
-sudo sed -i -E 's/#? (masterauth) (master-password>)/\1 hongduy/' /etc/redis/redis.conf
+sudo sed -i -E 's/#? (masterauth) (<master-password>)/\1 hongduy/' /etc/redis/redis.conf
 
 sudo service redis-server restart
